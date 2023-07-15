ce23420c761015f0c0cc056b5a0f52a1ab4d06d6
t0dd <swjasontodd@gmail.com>
2023-07-16 04:20:18 +0900

    feat: 레이아웃 분리

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="pt-20">
+    <slot />
+    <Navigator />
+    <ModalSort />
+    <ModalFilter />
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
