b3544a57baf03d753639a94048d025a3a3e930de
t0dd <swjasontodd@gmail.com>
2023-07-16 21:17:02 +0900

    feat: 파일 업로드 구현

---
+++ server/api/upload.post.ts
@@ -0,0 +1,53 @@
+import multer from 'multer'
+import { callNodeListener } from 'h3'
+
+export default defineEventHandler(async event => {
+  try {
+    let filePaths: string[] = []
+    let fileNames: string[] = []
+    const storage = multer.diskStorage({
+      destination: (req, file, cb) => {
+        cb(null, 'public/images')
+      },
+      filename: (req, file, cbd) => {
+        const filePath = `${Date.now()}-${file.originalname
+          .toLocaleLowerCase()
+          .replace(/\s+/g, '-')
+          .replace(/\./g, '-')}`
+        filePaths.push(filePath)
+        fileNames.push(file.originalname)
+        cbd(null, filePath)
+      },
+    })
+
+    const upload = multer({
+      storage: storage,
+      fileFilter: (req, file, cb) => {
+        if (
+          file.mimetype == 'image/png' ||
+          file.mimetype == 'image/jpeg' ||
+          file.mimetype === 'image/webp'
+        ) {
+          cb(null, true)
+        } else {
+          cb(new Error('Invalid file type'))
+        }
+      },
+    })
+
+    await callNodeListener(
+      // @ts-expect-error: Nuxt 3
+      upload.array('files', 5),
+      event.node.req,
+      event.node.res,
+    )
+
+    return filePaths.map(e => `/images/${e}`)
+  } catch (error) {
+    console.log(error)
+    return createError({
+      statusCode: 500,
+      statusMessage: 'Something went wrong.',
+    })
+  }
+})
