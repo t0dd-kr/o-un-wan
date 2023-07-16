f34c1206ce5a05f98ea7778c58005a460f770fc6
t0dd <swjasontodd@gmail.com>
2023-07-16 18:21:21 +0900

    feat: 로그인 로직 추가

--- server/api/signin.post.ts
+++ server/api/signin.post.ts
@@ -1,9 +1,23 @@
 import { zSigninPayload } from '@/entities/user.entity'
+import { genUid } from '@/server/utils'
+import { useColl } from '@/server/plugins/mongo'
 
 export default defineEventHandler(async event => {
   const { identify } = zSigninPayload.parse(await readBody(event))
 
   setCookie(event, 'identify', identify)
+  event.context.identify = identify
+
+  const USER = await useColl('user')
+
+  const user = await USER.findOne({ identify })
+
+  if (!user) {
+    await USER.insertOne({
+      identify,
+      uid: genUid(),
+    })
+  }
 
   return true
 })
